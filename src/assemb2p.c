472cf6eab2180baec5f116b754564fcb28de3965
Palash Sarkar <palashsarkar0007@gmail.com>
2017-11-13 05:59:07 +0530

    Update assemb2p.c

--- src/assemb2p.c
+++ src/assemb2p.c
@@ -1,3 +1,20 @@
+/*  -> Simple Console/CommandLine Assembler Application Using C11.
+ *  -> Project Type -- Self/Educational.
+ *  -> Institute -- University Institute Of Technology, Burdwan University.
+ *  -> Owner/Code file Designer :
+ *             @ Name - Palash Sarkar.
+ *             @ Department - Computer Science And Engineering.
+ *             @ Roll.Number - 2014_1038.
+ *             @ Email - palashsarkar0007@gmail.com.
+ *  -> Copyright Norms - Every piece of code given below 
+ *                       has been written by 'Palash Sarkar (Tj07)'©,
+ *                       and he holds the rights to the file. Not meant to be
+ *                       copied or tampered without prior permission
+ *                       from the author. 
+ * -> --Note-- - Some comments were added here and there throughout the source code
+ *               to verify the flow control and correctness of the program. Please
+ *               ignore them.
+ */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -34,6 +51,8 @@ int main()
   writeto_symfile();
   return (0);
 }
+
+//Uncomment this module/subroutine one for filling in the assembler program and the machine-opcode table and then recomment it!!
 
 /*void write()
 {
@@ -316,6 +335,7 @@ void writeto_symfile()
         }
         cch=' ';
         fputc(cch,fr);
+        //itoa(btw->offset,wr,10); --don't use for C11/C99, as it is not a standard lib function, use 'snprintf' instead.
         snprintf(wr,10,"%d",btw->offset);
         kt=0;
         while(wr[kt]!='\0')
